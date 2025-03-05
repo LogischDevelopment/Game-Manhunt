42b71c84f7e2a4685abdd924bd3aecf9e417d656
Timo <timokeutgen@gmx.de>
2025-03-05 16:09:06 +0100

    Fixes

--- src/main/java/tv/logisch/manhunt/listener/PlayerQuitListener.java
+++ src/main/java/tv/logisch/manhunt/listener/PlayerQuitListener.java
@@ -8,6 +8,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerQuitEvent;
+import tv.logisch.manhunt.enums.GameState;
 import tv.logisch.manhunt.manager.GameManager;
 
 public class PlayerQuitListener implements Listener {
@@ -21,6 +22,8 @@ public class PlayerQuitListener implements Listener {
         Bukkit.getOnlinePlayers().forEach(p -> {
             p.sendMessage(Component.text("§8[§c-§8] §7" + e.getPlayer().getName() + " §8(§7" + role + "§8)"));
         });
+
+        if(!GameManager.state().equals(GameState.WAITING)) return;
 
         for(OfflinePlayer player : Bukkit.getWhitelistedPlayers()) {
             if(player.getPlayer() != null && Bukkit.getOnlinePlayers().contains(player.getPlayer()) && player.getPlayer().getGameMode().equals(GameMode.SURVIVAL) && !player.getPlayer().getUniqueId().equals(e.getPlayer().getUniqueId())) {
