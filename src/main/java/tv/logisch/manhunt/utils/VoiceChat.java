e39ec0ca904698d1d019d0c0105a9facffef1b9b
Timo <timokeutgen@gmx.de>
2025-07-03 22:24:19 +0200

    Remove unused `findFreePortInRange` method from `VoiceChat` utility and improve UFW port rules update

--- src/main/java/tv/logisch/manhunt/utils/VoiceChat.java
+++ src/main/java/tv/logisch/manhunt/utils/VoiceChat.java
@@ -3,20 +3,9 @@ package tv.logisch.manhunt.utils;
 import org.bukkit.Bukkit;
 
 import java.io.*;
-import java.net.ServerSocket;
 import java.util.Properties;
 
 public class VoiceChat {
-
-    public static int findFreePortInRange(int min, int max) {
-        for (int port = min; port <= max; port++) {
-            try (ServerSocket ignored = new ServerSocket(port)) {
-                return port;
-            } catch (IOException ignored) {
-            }
-        }
-        throw new RuntimeException("Kein freier Port im Bereich " + min + "-" + max);
-    }
 
     public static void setVoiceChatPort(int port) {
         File configFile = new File(Bukkit.getPluginsFolder().getPath() + "/voicechat/voicechat-server.properties");
@@ -44,10 +33,12 @@ public class VoiceChat {
     }
 
     public static void allowUfwPort(int port) {
+        executeCommand("sudo ufw allow " + port);
         executeCommand("sudo ufw allow " + port + "/udp");
     }
 
     public static void denyUfwPort(int port) {
+        executeCommand("sudo ufw delete allow " + port);
         executeCommand("sudo ufw delete allow " + port + "/udp");
     }
 
