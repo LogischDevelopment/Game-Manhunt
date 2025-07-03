acc9b302322af82bd0f6e05cb67627626ed6f49a
Timo <timokeutgen@gmx.de>
2025-07-03 22:06:24 +0200

    Add enhanced countdown alerts for final seconds of server shutdown

--- src/main/java/tv/logisch/manhunt/manager/GameManager.java
+++ src/main/java/tv/logisch/manhunt/manager/GameManager.java
@@ -141,6 +141,10 @@ public class GameManager {
                 Bukkit.getOnlinePlayers().forEach(player -> {
                     player.setLevel(seconds[0]);
                     player.setExp(seconds[0] / 15f);
+                    if(seconds[0] == 5 || seconds[0] <= 3) {
+                        player.sendMessage(Component.text("§8[§bManhunt§8] §7Der Server stoppt in §b" + seconds[0] + " §7Sekunden!"));
+                        player.playSound(player, Sound.BLOCK_NOTE_BLOCK_PLING, 1, 1);
+                    }
                 });
             }
         };
